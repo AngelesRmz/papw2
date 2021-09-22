c5e846ed0dcc4e934a1538f3975d1765f3a60dd3
AngelesRmz <91154737+AngelesRmz@users.noreply.github.com>
2021-09-21 19:53:22 -0500

    Add files via upload

---
+++ public/js/login.js
@@ -0,0 +1,19 @@
+// $('document').ready(function(){
+
+//     $('span.help-block').css("opacity", 0);
+    
+//         $('#Scorreo').keyup(function(){
+//         var regex = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
+
+//             if (regex.test($('#Scorreo').val().trim())) {
+//                 $('#eEmail.error2').css("opacity", 100);
+//                 $('#eEmail.error2').css("color", "green");
+//                 $('#eEmail.error2').text('Correo válido');
+//             } else {
+//                 $('#eEmail.error2').css("opacity", 100);
+
+//                 $('#eEmail.error2').text('Correo no válido');
+//             }
+//        });
+
+// });
